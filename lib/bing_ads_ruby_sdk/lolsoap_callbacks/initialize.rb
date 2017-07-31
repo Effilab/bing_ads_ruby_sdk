71405dc51d23e886e91a4a430b5fcfe481e49d51
sami <sami@effilab-local.com>
2017-07-31 17:16:01 +0200

    Move callbacks in an initializer

---
+++ lib/bing_ads_ruby_sdk/lolsoap_callbacks/initialize.rb
@@ -0,0 +1,15 @@
+request_callback = LolSoap::Callbacks.new
+request_callback.for('hash_params.before_build') << lambda do |args, node, type|
+  matcher = type.elements.keys.map { |name| name.tr('_', '').downcase }
+  args.each do |h|
+    if h[:name] == 'advertiser_account'
+      node.add_namespace_definition('xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+      h[:args] << { 'xsi:type' => 'ns0:AdvertiserAccount' }
+      h[:name] = 'Account'
+    else
+      found_at = matcher.index(h[:name].tr('_', '').downcase)
+      h[:name] = type.elements.keys[found_at] if found_at
+    end
+  end
+  args.sort_by! { |h| type.elements.keys.index(h[:name]) || 1 / 0.0 }
+end
