0ed0e077cd6bdef635bb9667f9380d6ed5ed4ad9
Daniel Barlow <danielmbarlow@gmail.com>
2017-08-10 15:42:39 +0200

    style changes and refactors for CallBackManager

--- lib/bing_ads_ruby_sdk/soap_callback_manager.rb
+++ lib/bing_ads_ruby_sdk/soap_callback_manager.rb
@@ -2,57 +2,72 @@
 
 require 'bing_ads_ruby_sdk/utils'
 
-# Handles of LolSoap callbacks
-class BingAdsRubySdk::SoapCallbackManager
-  def self.register_callbacks
-    # Modify the request data before it is sent via the SOAP client
-    BingAdsRubySdk.request_callback.for('hash_params.before_build') << lambda do |args, _node, type|
-      before_build(args, type)
+module BingAdsRubySdk
+  # Handles of LolSoap callbacks
+  class SoapCallbackManager
+    def self.register_callbacks
+      # Modify the request data before it is sent via the SOAP client
+      BingAdsRubySdk.request_callback
+                    .for('hash_params.before_build') <<
+        lambda do |args, _node, type|
+          before_build(args, type)
+        end
+
+      # Modify the response data whilst it is being processed by the SOAP client
+      BingAdsRubySdk.response_callback
+                    .for('hash_builder.after_children_hash') <<
+        lambda do |hash, _node, _type|
+          after_children_hash(hash)
+        end
     end
 
-    # Modify the response data whilst it is being processed by the SOAP client
-    BingAdsRubySdk.response_callback.for('hash_builder.after_children_hash') << lambda do |hash, _node, _type|
-      after_children_hash(hash)
-    end
-  end
+    def self.before_build(args, type)
+      convert_element_names(args, type)
 
-  def self.before_build(args, type)
-    # Fuzzy matching for elements names
-    matcher = type.elements.keys.map { |name| name.tr('_', '').downcase }
+      # Sorts the request data on the wsdl order
+      args.sort_by! { |h| type.elements.keys.index(h[:name]) || 1 / 0.0 }
 
-    args.each do |h|
-      found_at = matcher.index(h[:name].tr('_', '').downcase)
-      h[:name] = type.elements.keys[found_at] if found_at
+      mark_null_types_with_nil(args, type)
     end
 
-    # Sorts the request data on the wsdl order
-    args.sort_by! { |h| type.elements.keys.index(h[:name]) || 1 / 0.0 }
+    def self.after_children_hash(hash)
+      hash.keys.each do |k|
+        val = hash.delete(k)
 
-    mark_null_types_with_nil(args, type)
-  end
+        # Convert values of type long to integer.
+        # Removes unecessary :long hash key.
+        # TODO : use the type from wsdl instead ?
+        if val.is_a?(Hash) && val[:long].is_a?(Array)
+          val = val[:long].map(&:to_i)
+        end
 
-  def self.after_children_hash(hash)
-    hash.keys.each do |k|
-      val = hash.delete(k)
-
-      # Convert values of type long to integer.
-      # Removes unecessary :long hash key.
-      # TODO : use the type from wsdl instead ?
-      val = val[:long].map(&:to_i) if val.is_a?(Hash) && val[:long].is_a?(Array)
-
-      # Add the value to the return hash using a symbol as a key instead of the
-      # default CamelCase string
-      hash[BingAdsRubySdk::Utils.snakize(k).to_sym] = val
+        # Add the value to the return hash using a symbol as a key instead
+        # of the default CamelCase string
+        hash[BingAdsRubySdk::Utils.snakize(k).to_sym] = val
+      end
     end
-  end
 
-  def self.null_type_fields(args, type)
-    args.select do |arg|
-      type.elements[arg[:name]].type.is_a?(LolSoap::WSDL::NullType) && arg[:args].compact.empty?
+    def self.convert_element_names(argument_hashes, type)
+      # Fuzzy matching for element names
+      matcher = type.elements.keys.map { |name| name.tr('_', '').downcase }
+
+      argument_hashes.each do |h|
+        found_at = matcher.index(h[:name].tr('_', '').downcase)
+        h[:name] = type.elements.keys[found_at] if found_at
+      end
     end
-  end
 
-  def self.mark_null_types_with_nil(args, type)
-    null_type_fields(args, type).each { |arg| arg[:args] << { 'xsi:nil' => true } }
+    def self.null_type_fields(args, type)
+      args.select do |arg|
+        type.elements[arg[:name]].type.is_a?(LolSoap::WSDL::NullType) &&
+          arg[:args].compact.empty?
+      end
+    end
+
+    def self.mark_null_types_with_nil(args, type)
+      null_type_fields(args, type).each do |arg|
+        arg[:args] << { 'xsi:nil' => true }
+      end
+    end
   end
 end
