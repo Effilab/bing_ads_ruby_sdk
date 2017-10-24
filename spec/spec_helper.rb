0e61a61436493c992da9ac4cfc84f384ba22aaff
Damian Le Nouaille <dam@dln.name>
2017-10-24 16:11:18 +0200

    fix spec config for example, still 5 failing but running

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,4 @@ warn_level = $VERBOSE
 $VERBOSE = nil
 BingAdsRubySdk::Configuration::ENVIRONMENTS = %i[test].freeze
 BingAdsRubySdk::Configuration::VERSIONS     = %i[v11].freeze
-BingAdsRubySdk::Configuration::CACHE_BASE   = Dir.tmpdir
-BingAdsRubySdk::Configuration::CONF_PATH    = File.join(__dir__, 'fixtures')
 $VERBOSE = warn_level
