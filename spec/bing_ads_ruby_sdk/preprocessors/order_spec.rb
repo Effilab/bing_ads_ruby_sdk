8f4f297bc0baf30de71b925124517b7a1eba44a3
Daniel Barlow <danielmbarlow@gmail.com>
2021-11-15 15:29:32 +0100

    Convert some let blocks to do...end

--- spec/bing_ads_ruby_sdk/preprocessors/order_spec.rb
+++ spec/bing_ads_ruby_sdk/preprocessors/order_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe BingAdsRubySdk::Preprocessors::Order do
       SpecHelpers.wrapper(:customer_management, "SignupCustomer")
     end
 
-    let(:unordered_params) {
+    let(:unordered_params) do
       {
         "Account" => {
           "Name" => "test account",
@@ -25,9 +25,9 @@ RSpec.describe BingAdsRubySdk::Preprocessors::Order do
         },
         "ParentCustomerId" => "1234"
       }
-    }
+    end
 
-    let(:ordered_params) {
+    let(:ordered_params) do
       {
         "Customer" => {
           "Industry" => "industry",
@@ -41,7 +41,7 @@ RSpec.describe BingAdsRubySdk::Preprocessors::Order do
         },
         "ParentCustomerId" => "1234"
       }
-    }
+    end
 
     it("orders") { action }
   end
@@ -51,7 +51,7 @@ RSpec.describe BingAdsRubySdk::Preprocessors::Order do
       SpecHelpers.wrapper(:campaign_management, "UpdateUetTags")
     end
 
-    let(:unordered_params) {
+    let(:unordered_params) do
       {
         "UetTags" => [
           {
@@ -63,9 +63,9 @@ RSpec.describe BingAdsRubySdk::Preprocessors::Order do
           }
         ]
       }
-    }
+    end
 
-    let(:ordered_params) {
+    let(:ordered_params) do
       {
         "UetTags" => [
           {
@@ -77,7 +77,7 @@ RSpec.describe BingAdsRubySdk::Preprocessors::Order do
           }
         ]
       }
-    }
+    end
 
     it("orders") { action }
   end
@@ -87,7 +87,7 @@ RSpec.describe BingAdsRubySdk::Preprocessors::Order do
       SpecHelpers.wrapper(:campaign_management, "AddConversionGoals")
     end
 
-    let(:unordered_params) {
+    let(:unordered_params) do
       {
         "ConversionGoals" => [
           {
@@ -97,16 +97,16 @@ RSpec.describe BingAdsRubySdk::Preprocessors::Order do
               "ConversionWindowInMinutes" => 43200,
               "CountType" => "Unique",
               "Name" => "contact_form",
-              "Revenue" => {"Type" => "NoValue"},
+              "Revenue" => { "Type" => "NoValue" },
               "Type" => "Event",
               "TagId" => 26003317
             }
           }
         ]
       }
-    }
+    end
 
-    let(:ordered_params) {
+    let(:ordered_params) do
       {
         "ConversionGoals" => [
           {
@@ -114,7 +114,7 @@ RSpec.describe BingAdsRubySdk::Preprocessors::Order do
               "ConversionWindowInMinutes" => 43200,
               "CountType" => "Unique",
               "Name" => "contact_form",
-              "Revenue" => {"Type" => "NoValue"},
+              "Revenue" => { "Type" => "NoValue" },
               "TagId" => 26003317,
               "Type" => "Event",
               "ActionExpression" => "contact_form",
@@ -123,7 +123,7 @@ RSpec.describe BingAdsRubySdk::Preprocessors::Order do
           }
         ]
       }
-    }
+    end
 
     it("orders") { action }
   end
