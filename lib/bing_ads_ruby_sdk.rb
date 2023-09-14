1068bf766b6c6cad78efb80a711f7c9b7a467bda
tdutreui-solocal <tdutreuilh@solocal.com>
2023-09-14 15:31:25 +0200

    reverted syntax to 3.0

--- lib/bing_ads_ruby_sdk.rb
+++ lib/bing_ads_ruby_sdk.rb
@@ -21,9 +21,9 @@ module BingAdsRubySdk
     yield(config)
   end
 
-  def self.log(level, *, &block)
+  def self.log(level, *args, &block)
     return unless config.log
-    config.logger.send(level, *, &block)
+    config.logger.send(level, *args, &block)
   end
 
   def self.root_path
