651bd9030de066b075751e6c23880905c24b5349
Daniel Barlow <danielmbarlow@gmail.com>
2025-01-27 11:35:23 +0100

    EFFTECH-3386 test and configure json_api.rb

--- lib/bing_ads_ruby_sdk/json_api.rb
+++ lib/bing_ads_ruby_sdk/json_api.rb
@@ -9,27 +9,29 @@ module BingAdsRubySdk
     attr_reader :headers
 
     URL_MAP = {
-      campaign_management: "https://campaign.api.bingads.microsoft.com/CampaignManagement/v13/"
+      campaign_management: "https://campaign.api.%{sandbox}bingads.microsoft.com/CampaignManagement/%{version}/"
     }
 
     # @param developer_token
     # @param client_id
-    # @param oauth_store instance of a store
+    # @param client_secret
+    # @param oauth_store instance of a store, used to get OAuth credentials
     # @param version [Symbol] API version, used to choose version
     # @param environment [Symbol]
     # @option environment [Symbol] :production Use the production environment
     # @option environment [Symbol] :sandbox Use the sandbox environment
-    # @param developer_token
-    # @param client_id
     def initialize(
       developer_token:,
       client_id:,
+      client_secret:,
       oauth_store:,
       version: DEFAULT_SDK_VERSION,
-      environment: :production,
-      client_secret: nil)
+      environment: :production
+    )
+      # Validate version format
+      raise ArgumentError, "Invalid version format" unless version.to_s.match?(/^v\d+$/)
       @version = version
-      @environment = environment
+      @sandbox = (environment == :production) ? "" : "sandbox."
       @headers = {
         "DeveloperToken" => developer_token,
         "ClientId" => client_id,
@@ -40,8 +42,8 @@ module BingAdsRubySdk
       @auth_handler = ::BingAdsRubySdk::OAuth2::AuthorizationHandler.new(
         developer_token: developer_token,
         client_id: client_id,
-        store: oauth_store,
-        client_secret: client_secret
+        client_secret: client_secret,
+        store: oauth_store
       )
     end
 
@@ -62,7 +64,7 @@ module BingAdsRubySdk
 
     def build_service(service_name)
       Services::Json.new(
-        base_url: URL_MAP.fetch(service_name),
+        base_url: URL_MAP.fetch(service_name) % {version: @version, sandbox: @sandbox},
         headers: headers,
         auth_handler: auth_handler
       )
